ca30188da361df9d840dd49cfc70d97cc0a37f60
Jack Douglass <jackdouglass58@gmail.com>
2023-04-11 16:56:24 -0400

    updated core authentication

--- core/session.py
+++ core/session.py
@@ -129,7 +129,7 @@ class Sessions:
     def __init__(self):
         self.sessions = {}
 
-    def add_new_session(self, username: str, db: Database) -> None:
+    def add_new_session(self, username: str, db: Database, manager: bool) -> None:
         """
         Adds a new user session to the collection of sessions.
 
@@ -140,7 +140,7 @@ class Sessions:
         returns:
             - None
         """
-        self.sessions[username] = UserSession(username, db)
+        self.sessions[username] = UserSession(username, db, manager)
 
     def get_session(self, username: str) -> UserSession:
         """
